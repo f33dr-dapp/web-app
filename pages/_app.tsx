2b47f27e59e2b6cc7634d2deabb861af97077a36
Diogo Perillo <diogo.perillo@gmail.com>
2022-07-01 16:00:12 -0600

    Deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { DAppProvider } from '@usedapp/core'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import Layout from '../components/Layout'
 import dappConfig from '../config/dappConfig'
@@ -9,6 +10,17 @@ import theme from '../config/theme'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <title>F33Dr</title>
+        <meta
+          name="description"
+          content="Like Twitter, but on the Blockchain"
+        />
+        <meta name="author" content="F33Dr" />
+      </Head>
       <DAppProvider config={dappConfig}>
         <Layout>
           <Component {...pageProps} />
